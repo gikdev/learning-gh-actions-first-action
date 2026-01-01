693b1b2098ca7f2eefe86bf25d36b0d4504ab863
gikdev <m.m.bhrm85@gmail.com>
2026-01-01 15:05:46 +0330

    Revert "Change tests."
    This reverts commit 033c4434332fdc18249c9c64f17218ab3b3103ba.

--- tests/math.test.ts
+++ tests/math.test.ts
@@ -2,6 +2,6 @@ import { describe, it, expect } from 'vitest'
 
 describe('Simple math test', () => {
   it('1 + 1 should equal 2', () => {
-    expect(1 + 1).toBe(3)
+    expect(1 + 1).toBe(2)
   })
 })
